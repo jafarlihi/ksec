ff3f4db977e484617bcf8f9aee948d7a6634717e
Hikmat Jafarli <jafarlihi@gmail.com>
2022-07-14 17:26:33 +0400

    Resolve IDT entry handler address owner

--- kernel/ksec.c
+++ kernel/ksec.c
@@ -10,25 +10,31 @@
 
 enum {
   KSEC_A_UNSPEC,
-  KSEC_A_MSG,
+  KSEC_A_STR,
   KSEC_A_BIN,
+  KSEC_A_U64,
   __KSEC_A_MAX,
 };
 #define KSEC_A_MAX (__KSEC_A_MAX - 1)
 
 enum {
   KSEC_C_UNSPEC,
+  KSEC_C_IS_KERNEL_ADDR,
+  KSEC_C_IS_MODULE_ADDR,
   KSEC_C_GET_IDT_ENTRIES,
   __KSEC_C_MAX,
 };
 #define KSEC_C_MAX (__KSEC_C_MAX - 1)
 
 static struct nla_policy ksec_genl_policy[KSEC_A_MAX + 1] = {
-  [KSEC_A_MSG] = { .type = NLA_NUL_STRING },
+  [KSEC_A_STR] = { .type = NLA_NUL_STRING },
   [KSEC_A_BIN] = { .type = NLA_BINARY },
+  [KSEC_A_U64] = { .type = NLA_U64 },
 };
 
 static int get_idt_entries(struct sk_buff *, struct genl_info *);
+static int is_kernel_addr(struct sk_buff *, struct genl_info *);
+static int is_module_addr(struct sk_buff *, struct genl_info *);
 
 static struct genl_ops ksec_ops[] = {
   {
@@ -36,6 +42,18 @@ static struct genl_ops ksec_ops[] = {
     .flags = 0,
     .policy = ksec_genl_policy,
     .doit = get_idt_entries,
+  },
+  {
+    .cmd = KSEC_C_IS_KERNEL_ADDR,
+    .flags = 0,
+    .policy = ksec_genl_policy,
+    .doit = is_kernel_addr,
+  },
+  {
+    .cmd = KSEC_C_IS_MODULE_ADDR,
+    .flags = 0,
+    .policy = ksec_genl_policy,
+    .doit = is_module_addr,
   },
 };
 
@@ -46,12 +64,83 @@ static struct genl_family ksec_genl_family = {
   .version = 1,
   .maxattr = KSEC_A_MAX,
   .ops = ksec_ops,
-  .n_ops = 1,
+  .n_ops = 3,
 };
 
 typedef void *(*kallsyms_lookup_name_t)(const char *name);
 static kallsyms_lookup_name_t lookup;
-static unsigned __int128 *idt_table = NULL;
+
+static int is_kernel_addr(struct sk_buff *skb, struct genl_info *info) {
+  int (*core_kernel_text)(unsigned long addr) = lookup("core_kernel_text");
+
+  u64 va = nla_get_u64(info->attrs[KSEC_A_U64]);
+  u64 res = core_kernel_text(va);
+
+  struct sk_buff *reply_skb = genlmsg_new(sizeof(u64), GFP_KERNEL);
+  if (reply_skb == NULL) {
+    pr_err("An error occurred in %s()\n", __func__);
+    return -ENOMEM;
+  }
+
+  void *msg_head = genlmsg_put(reply_skb, info->snd_portid, info->snd_seq + 1, &ksec_genl_family, 0, KSEC_C_IS_KERNEL_ADDR);
+  if (msg_head == NULL) {
+    pr_err("An error occurred in %s()\n", __func__);
+    return -ENOMEM;
+  }
+
+  int rc = nla_put_u64_64bit(reply_skb, KSEC_A_U64, res, 0);
+  if (rc != 0) {
+    pr_err("An error occurred in %s()\n", __func__);
+    return -rc;
+  }
+
+  genlmsg_end(reply_skb, msg_head);
+  rc = genlmsg_reply(reply_skb, info);
+  if (rc != 0) {
+    pr_err("An error occurred in %s()\n", __func__);
+    return -rc;
+  }
+
+  return 0;
+}
+
+static int is_module_addr(struct sk_buff *skb, struct genl_info *info) {
+  struct module *(*get_module_from_addr)(unsigned long addr) = lookup("__module_address");
+
+  u64 va = nla_get_u64(info->attrs[KSEC_A_U64]);
+  struct module *module = get_module_from_addr(va);
+
+  char *module_name;
+  if (module) module_name = module->name;
+  else module_name = "";
+
+  struct sk_buff *reply_skb = genlmsg_new(sizeof(module_name), GFP_KERNEL);
+  if (reply_skb == NULL) {
+    pr_err("An error occurred in %s()\n", __func__);
+    return -ENOMEM;
+  }
+
+  void *msg_head = genlmsg_put(reply_skb, info->snd_portid, info->snd_seq + 1, &ksec_genl_family, 0, KSEC_C_IS_MODULE_ADDR);
+  if (msg_head == NULL) {
+    pr_err("An error occurred in %s()\n", __func__);
+    return -ENOMEM;
+  }
+
+  int rc = nla_put_string(reply_skb, KSEC_A_STR, module_name);
+  if (rc != 0) {
+    pr_err("An error occurred in %s()\n", __func__);
+    return -rc;
+  }
+
+  genlmsg_end(reply_skb, msg_head);
+  rc = genlmsg_reply(reply_skb, info);
+  if (rc != 0) {
+    pr_err("An error occurred in %s()\n", __func__);
+    return -rc;
+  }
+
+  return 0;
+}
 
 typedef struct {
   u16 offset_0_15;
@@ -77,6 +166,8 @@ typedef union {
 idt_entry_u_t idt_entry_info_arr[IDT_ENTRIES] = {0};
 
 static int get_idt_entries(struct sk_buff *skb, struct genl_info *info) {
+  unsigned __int128 *idt_table = (unsigned __int128 *)lookup("idt_table");
+
   for (int i = 0; i < IDT_ENTRIES; i++) {
     idt_entry_u_t entry;
     entry.scalar = idt_table[i];
@@ -97,7 +188,7 @@ static int get_idt_entries(struct sk_buff *skb, struct genl_info *info) {
 
   struct sk_buff *reply_skb = genlmsg_new(sizeof(idt_entry_u_t) * IDT_ENTRIES, GFP_KERNEL);
   if (reply_skb == NULL) {
-    pr_err("An error occurred in %s():\n", __func__);
+    pr_err("An error occurred in %s()\n", __func__);
     return -ENOMEM;
   }
 
@@ -140,7 +231,6 @@ static int __init modinit(void) {
   }
 
   resolve_kallsyms_lookup_name();
-  idt_table = (unsigned __int128 *)lookup("idt_table");
 
   return 0;
 }
--- user/src/main.rs
+++ user/src/main.rs
@@ -1,19 +1,22 @@
 use neli::neli_enum;
 
-pub const FAMILY_NAME: &str = "ksec";
+const FAMILY_NAME: &str = "ksec";
 
 #[neli_enum(serialized_type = "u8")]
-pub enum KsecCommand {
+enum KsecCommand {
     Unspec = 0,
-    GetIDTEntries = 1,
+    IsKernelAddr = 1,
+    IsModuleAddr = 2,
+    GetIDTEntries = 3,
 }
 impl neli::consts::genl::Cmd for KsecCommand {}
 
 #[neli_enum(serialized_type = "u16")]
-pub enum KsecAttribute {
+enum KsecAttribute {
     Unspec = 0,
-    Msg = 1,
+    Str = 1,
     Bin = 2,
+    U64 = 3,
 }
 impl neli::consts::genl::NlAttrType for KsecAttribute {}
 
@@ -22,17 +25,18 @@ use neli::{
         nl::{NlmF, NlmFFlags},
         socket::NlFamily,
     },
-    genl::{Genlmsghdr},
+    genl::{Genlmsghdr, Nlattr},
     nl::{NlPayload, Nlmsghdr},
     socket::NlSocketHandle,
     types::{Buffer, GenlBuffer},
 };
-use x86_64::VirtAddr;
+use x86_64::{VirtAddr};
 use std::process;
 use clap::Parser;
+use std::fmt;
 use x86_64::structures::idt::{Entry, HandlerFunc};
 
-fn send_netlink_message(cmd: KsecCommand) -> Nlmsghdr<u16, Genlmsghdr<KsecCommand, KsecAttribute>> {
+fn send_netlink_message(cmd: KsecCommand, attrs: GenlBuffer<KsecAttribute, Buffer>) -> Nlmsghdr<u16, Genlmsghdr<KsecCommand, KsecAttribute>> {
     let mut sock = NlSocketHandle::connect(
         NlFamily::Generic,
         Some(0),
@@ -40,7 +44,6 @@ fn send_netlink_message(cmd: KsecCommand) -> Nlmsghdr<u16, Genlmsghdr<KsecComman
     ).unwrap();
 
     let family_id = sock.resolve_genl_family(FAMILY_NAME).unwrap();
-    let attrs: GenlBuffer<KsecAttribute, Buffer> = GenlBuffer::new();
     let gnmsghdr = Genlmsghdr::new(
         cmd,
         1,
@@ -71,20 +74,73 @@ struct Args {
     get_idt_entries: bool,
 }
 
+fn virtaddr_to_nlattr(va: VirtAddr) -> GenlBuffer<KsecAttribute, Buffer> {
+    let mut attrs: GenlBuffer<KsecAttribute, Buffer> = GenlBuffer::new();
+    attrs.push(
+        Nlattr::new(
+            false,
+            false,
+            KsecAttribute::U64,
+            va.as_u64(),
+        ).unwrap(),
+    );
+    return attrs;
+}
+
+enum AddrOwner {
+    Unspec = 0,
+    Kernel = 1,
+    Module = 2,
+    Process = 3,
+}
+
+impl fmt::Display for AddrOwner {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            AddrOwner::Unspec => write!(f, "Unknown"),
+            AddrOwner::Kernel => write!(f, "Kernel"),
+            AddrOwner::Module => write!(f, "Module"),
+            AddrOwner::Process => write!(f, "Process"),
+        }
+    }
+}
+
+fn get_virtaddr_owner(va: VirtAddr) -> (AddrOwner, Option<String>) {
+    let attrs = virtaddr_to_nlattr(va);
+    let res = send_netlink_message(KsecCommand::IsKernelAddr, attrs);
+    let attr_handle = res.get_payload().unwrap().get_attr_handle();
+    let attr = attr_handle.get_attr_payload_as_with_len::<&[u8]>(KsecAttribute::U64).unwrap();
+    if !attr.iter().any(|&v| v > 0) {
+        let attrs = virtaddr_to_nlattr(va);
+        let res = send_netlink_message(KsecCommand::IsModuleAddr, attrs);
+        let attr_handle = res.get_payload().unwrap().get_attr_handle();
+        let attr = attr_handle.get_attr_payload_as_with_len::<String>(KsecAttribute::Str).unwrap();
+        if attr.is_empty() {
+            (AddrOwner::Process, None) // TODO: Unspec or process? Process name?
+        } else {
+            (AddrOwner::Module, Some(attr))
+        }
+    } else {
+        (AddrOwner::Kernel, None)
+    }
+}
+
 fn main() {
     let args = Args::parse();
 
     if args.get_idt_entries {
-        let res = send_netlink_message(KsecCommand::GetIDTEntries);
+        let attrs: GenlBuffer<KsecAttribute, Buffer> = GenlBuffer::new();
+        let res = send_netlink_message(KsecCommand::GetIDTEntries, attrs);
         let attr_handle = res.get_payload().unwrap().get_attr_handle();
-        let attr = attr_handle
-            .get_attr_payload_as_with_len::<&[u8]>(KsecAttribute::Bin)
-            .unwrap();
+        let attr = attr_handle.get_attr_payload_as_with_len::<&[u8]>(KsecAttribute::Bin).unwrap();
 
         let entries = unsafe { std::slice::from_raw_parts(attr.as_ptr() as *const Entry<HandlerFunc>, 256) };
+
         for i in 0..256 {
+            let owner = get_virtaddr_owner(entries[i].handler_addr());
             if entries[i].handler_addr() != VirtAddr::new(0) {
-                println!("{}: {:?}", i, entries[i]);
+                println!("{}: {:?} -> {}", i, entries[i], owner.0.to_string());
+                // TODO: Print process or module name
             }
         }
     }
