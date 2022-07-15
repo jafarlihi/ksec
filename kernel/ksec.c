e7d49a9fac5fc80b058a0c3995e503958ad8631f
Hikmat Jafarli <jafarlihi@gmail.com>
2022-07-15 18:48:00 +0400

    Add 'read' backend

--- kernel/ksec.c
+++ kernel/ksec.c
@@ -13,7 +13,8 @@ enum {
   KSEC_A_UNSPEC,
   KSEC_A_STR,
   KSEC_A_BIN,
-  KSEC_A_U64,
+  KSEC_A_U64_0,
+  KSEC_A_U64_1,
   __KSEC_A_MAX,
 };
 #define KSEC_A_MAX (__KSEC_A_MAX - 1)
@@ -26,6 +27,7 @@ enum {
   KSEC_C_GET_SYSCALLS,
   KSEC_C_GET_MODULES,
   KSEC_C_GET_SYMBOL_ADDR,
+  KSEC_C_READ,
   __KSEC_C_MAX,
 };
 #define KSEC_C_MAX (__KSEC_C_MAX - 1)
@@ -33,13 +35,15 @@ enum {
 static struct nla_policy ksec_genl_policy[KSEC_A_MAX + 1] = {
   [KSEC_A_STR] = { .type = NLA_NUL_STRING },
   [KSEC_A_BIN] = { .type = NLA_BINARY },
-  [KSEC_A_U64] = { .type = NLA_U64 },
+  [KSEC_A_U64_0] = { .type = NLA_U64 },
+  [KSEC_A_U64_1] = { .type = NLA_U64 },
 };
 
 static int get_idt_entries(struct sk_buff *, struct genl_info *);
 static int get_syscalls(struct sk_buff *, struct genl_info *);
 static int get_modules(struct sk_buff *, struct genl_info *);
 static int get_symbol_addr(struct sk_buff *, struct genl_info *);
+static int read(struct sk_buff *, struct genl_info *);
 static int is_kernel_addr(struct sk_buff *, struct genl_info *);
 static int is_module_addr(struct sk_buff *, struct genl_info *);
 
@@ -80,6 +84,12 @@ static struct genl_ops ksec_ops[] = {
     .policy = ksec_genl_policy,
     .doit = get_symbol_addr,
   },
+  {
+    .cmd = KSEC_C_READ,
+    .flags = 0,
+    .policy = ksec_genl_policy,
+    .doit = read,
+  },
 };
 
 static struct genl_family ksec_genl_family = {
@@ -89,7 +99,7 @@ static struct genl_family ksec_genl_family = {
   .version = 1,
   .maxattr = KSEC_A_MAX,
   .ops = ksec_ops,
-  .n_ops = 6,
+  .n_ops = 7,
 };
 
 typedef void *(*kallsyms_lookup_name_t)(const char *name);
@@ -98,7 +108,7 @@ static kallsyms_lookup_name_t lookup;
 static int is_kernel_addr(struct sk_buff *skb, struct genl_info *info) {
   int (*core_kernel_text)(unsigned long addr) = lookup("core_kernel_text");
 
-  u64 va = nla_get_u64(info->attrs[KSEC_A_U64]);
+  u64 va = nla_get_u64(info->attrs[KSEC_A_U64_0]);
   u64 res = core_kernel_text(va);
 
   struct sk_buff *reply_skb = genlmsg_new(sizeof(u64), GFP_KERNEL);
@@ -113,7 +123,7 @@ static int is_kernel_addr(struct sk_buff *skb, struct genl_info *info) {
     return -ENOMEM;
   }
 
-  int rc = nla_put_u64_64bit(reply_skb, KSEC_A_U64, res, 0);
+  int rc = nla_put_u64_64bit(reply_skb, KSEC_A_U64_0, res, 0);
   if (rc != 0) {
     pr_err("An error occurred in %s()\n", __func__);
     return -rc;
@@ -132,7 +142,7 @@ static int is_kernel_addr(struct sk_buff *skb, struct genl_info *info) {
 static int is_module_addr(struct sk_buff *skb, struct genl_info *info) {
   struct module *(*get_module_from_addr)(unsigned long addr) = lookup("__module_address");
 
-  u64 va = nla_get_u64(info->attrs[KSEC_A_U64]);
+  u64 va = nla_get_u64(info->attrs[KSEC_A_U64_0]);
   struct module *module = get_module_from_addr(va);
 
   char *module_name;
@@ -293,7 +303,39 @@ static int get_symbol_addr(struct sk_buff *skb, struct genl_info *info) {
     return -ENOMEM;
   }
 
-  int rc = nla_put(reply_skb, KSEC_A_U64, sizeof(u64), (void *)addr);
+  int rc = nla_put(reply_skb, KSEC_A_U64_0, sizeof(u64), (void *)addr);
+  if (rc != 0) {
+    pr_err("An error occurred in %s()\n", __func__);
+    return -rc;
+  }
+
+  genlmsg_end(reply_skb, msg_head);
+  rc = genlmsg_reply(reply_skb, info);
+  if (rc != 0) {
+    pr_err("An error occurred in %s()\n", __func__);
+    return -rc;
+  }
+
+  return 0;
+}
+
+static int read(struct sk_buff *skb, struct genl_info *info) {
+  u64 va = nla_get_u64(info->attrs[KSEC_A_U64_0]);
+  u64 len = nla_get_u64(info->attrs[KSEC_A_U64_1]);
+
+  struct sk_buff *reply_skb = genlmsg_new(len, GFP_KERNEL);
+  if (reply_skb == NULL) {
+    pr_err("An error occurred in %s()\n", __func__);
+    return -ENOMEM;
+  }
+
+  void *msg_head = genlmsg_put(reply_skb, info->snd_portid, info->snd_seq + 1, &ksec_genl_family, 0, KSEC_C_READ);
+  if (msg_head == NULL) {
+    pr_err("An error occurred in %s()\n", __func__);
+    return -ENOMEM;
+  }
+
+  int rc = nla_put(reply_skb, KSEC_A_BIN, len, (void *)va);
   if (rc != 0) {
     pr_err("An error occurred in %s()\n", __func__);
     return -rc;
